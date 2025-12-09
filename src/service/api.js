969d661f968c0488104639d0cfe0b96b1a104c64
longbachit-lgtm <longbach.it@gmail.com>
2025-12-09 14:00:22 +0700

    update api

--- src/service/api.js
+++ src/service/api.js
@@ -1,5 +1,5 @@
-let API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
-
+// let API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
+const API_BASE_URL = 'http://72.62.66.182:5000';
 console.log('API_BASE_URL hiện tại là:', API_BASE_URL);
 
 // Helper function để lấy token từ localStorage
